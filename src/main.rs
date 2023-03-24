6b438a17d590478f520b6244a7eedfee4106c950
gbPagano <guilhermebpagano@gmail.com>
2023-03-24 01:19:36 -0300

    feature: background and clamped borders

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use bevy::{prelude::*, sprite::MaterialMesh2dBundle};
+use bevy::{prelude::*, sprite::MaterialMesh2dBundle, render::camera::ScalingMode};
 use bevy_editor_pls::prelude::*;
 use bevy_pancam::{PanCam, PanCamPlugin};
 
@@ -28,22 +28,39 @@ struct Velocity(Vec2);
 #[derive(Component)]
 struct Acceleration(Vec2);
 
+#[derive(Component)]
+struct Mass(u64);
+
 
 fn setup(
     mut commands: Commands,
+    asset_server: Res<AssetServer>,
     mut meshes: ResMut<Assets<Mesh>>,
     mut materials: ResMut<Assets<ColorMaterial>>,
 ) {
-    commands.spawn((Camera2dBundle::default(), PanCam::default()));
+    let background_image = asset_server.load("background_2.jpg");
+    let mut cam = Camera2dBundle::default();
+    cam.projection.scaling_mode = ScalingMode::FixedVertical(2500.0);
 
+    commands.spawn((cam, PanCam{
+            // max_scale: Some(4.),
+            min_scale: 0.3,
+            min_x: Some(-6016./2.0),
+            max_x: Some(6016./2.0),
+            min_y: Some(-4016./2.0),
+            max_y: Some(4016./2.0),
+            ..default()
+        }));
+
+    commands.spawn(SpriteBundle {
+        texture: background_image,
+        ..default()
+    });
     // Circle
     commands.spawn(MaterialMesh2dBundle {
-        mesh: meshes.add(shape::Circle::new(100.).into()).into(),
+        mesh: meshes.add(shape::Circle::new(300.).into()).into(),
         material: materials.add(ColorMaterial::from(Color::PURPLE)),
-        transform: Transform::from_translation(Vec3::new(0., 0., 0.)),
+        transform: Transform::from_translation(Vec3::new(0., 0., 1.)),
         ..default()
     });
 }
-
-
-
