9422b4314ea348726fc5fdbe537f7c275992f287
gbPagano <guilhermebpagano@gmail.com>
2023-04-09 23:38:08 -0300

    gravity

--- src/main.rs
+++ src/main.rs
@@ -5,23 +5,25 @@ use bevy_pancam::{PanCam, PanCamPlugin};
 
 const BACKGROUND_COLOR: Color = Color::BLACK;
 const TIME_STEP: f32 = 1. / 60.;
+const GRAVITATIONAL_CONSTANT: f32 = 0.667; 
 
 fn main() {
     App::new()
         .add_plugins(DefaultPlugins)
         .insert_resource(ClearColor(BACKGROUND_COLOR))
         .insert_resource(FixedTime::new_from_secs(TIME_STEP))
-        .add_plugin(EditorPlugin)
-        .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
-        .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
+        // .add_plugin(EditorPlugin)
+        // .add_plugin(bevy::diagnostic::FrameTimeDiagnosticsPlugin)
+        // .add_plugin(bevy::diagnostic::EntityCountDiagnosticsPlugin)
         .add_plugin(PanCamPlugin::default())
         .add_startup_system(setup)
         .add_systems(
             (
-                apply_acceleration,
+                apply_gravity,
+                apply_acceleration.after(apply_gravity),
                 apply_velocity.after(apply_acceleration),
-                // clean_trace.after(apply_velocity),
-                // spawn_trace.after(clean_trace),
+                clean_trace.after(apply_velocity),
+                spawn_trace.after(clean_trace),
             )
                 .in_schedule(CoreSchedule::FixedUpdate),
         )
@@ -40,14 +42,18 @@ struct Position(Vec2);
 #[derive(Component, Deref, DerefMut)]
 struct Velocity(Vec2);
 
-#[derive(Component, Deref, DerefMut)]
+#[derive(Component, Deref, DerefMut, Debug)]
 struct Acceleration(Vec2);
 
-#[derive(Component)]
-struct Mass(u64);
+#[derive(Component, Debug)]
+struct Mass {
+    value: u64,
+}
 
-#[derive(Component)]
-struct Radius(f32);
+#[derive(Component, Debug)]
+struct Radius {
+    value: f32,
+}
 
 #[derive(Component, Deref, DerefMut)]
 struct Trace(BoundedVecDeque<Vec2>);
@@ -111,8 +117,8 @@ fn setup(
             ..default()
         },
         Planet,
-        Mass(34),
-        Radius(planet_radius),
+        Mass { value: 1000000 },
+        Radius { value: planet_radius },
         Position(Vec2::new(0., 0.)),
     ));
 
@@ -121,20 +127,60 @@ fn setup(
         MaterialMesh2dBundle {
             mesh: meshes.add(shape::Circle::new(15.).into()).into(),
             material: materials.add(ColorMaterial::from(Color::GREEN)),
-            transform: Transform::from_xyz(planet_radius, planet_radius, 1.),
+            transform: Transform::from_xyz(0., planet_radius + 200., 1.),
             ..default()
         },
         Ball,
-        Velocity(Vec2::new(50., 0.)),
-        Acceleration(Vec2::new(0., 5.)),
+        Mass { value: 1},
+        Radius { value: 15.},
+        Velocity(Vec2::new(250., 0.)),
+        Acceleration(Vec2::new(0., 0.)),
         Trace(BoundedVecDeque::new(100)),
     ));
 }
 
+fn get_distance(point_a: &Vec3, point_b: Vec3) -> f32 {
+    let square_distance: f32 = (point_b.x - point_a.x).powf(2.) + (point_b.y - point_a.y).powf(2.);
+    square_distance.sqrt()
+}
+
+
+fn apply_gravity(
+    mut query_ball: Query<(&Transform, &mut Acceleration, &Mass), With<Ball>>,
+    query_planet: Query<&Mass, With<Planet>>,
+) {
+    let planet_mass = query_planet.single();
+    for (transform, mut acceleration, mass) in &mut query_ball {
+        println!("{:?}, {:?}", transform.translation, mass);
+        let mass_product = planet_mass.value * mass.value;
+        let mut distance = get_distance(&transform.translation, Vec3::new(0.,0., 1.));
+        if distance < 1. {
+            distance = 1.;
+        }
+        let distance_product = distance * distance;
+        let gravitational_force = GRAVITATIONAL_CONSTANT * mass_product as f32 / distance_product;
+
+        let cos = -transform.translation.x / distance;
+        let sen = -transform.translation.y / distance;
+    
+
+        let force_x = gravitational_force * cos;
+        let force_y = gravitational_force * sen;
+
+        println!("{} {}", force_x, force_y);
+
+        acceleration.x = force_x / mass.value as f32;
+        acceleration.y = force_y / mass.value as f32;
+
+    }
+}
+
+
 fn apply_acceleration(mut query: Query<(&mut Velocity, &Acceleration)>) {
     for (mut velocity, acceleration) in &mut query {
-        velocity.x += acceleration.x * TIME_STEP;
-        velocity.y += acceleration.y * TIME_STEP;
+        println!("{:?}", acceleration);
+        velocity.x += acceleration.x;
+        velocity.y += acceleration.y;
     }
 }
 
